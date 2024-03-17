2ba513e7ed82c3e1b5aeaaff6d64d96be4279f29
daeldev <121415003+samuelalmeida345@users.noreply.github.com>
2024-03-17 19:07:54 -0300

    Adicionando comentários
    Adicionando comentários à cada ação.

--- testegit/src/main/java/com/mycompany/testegit/Testegit.java
+++ testegit/src/main/java/com/mycompany/testegit/Testegit.java
@@ -13,20 +13,20 @@ import java.util.Scanner;
 public class Testegit {
 
     public static void main(String[] args) {
-       
+       //variáveis
         int n1;
         int n2;
-        
+        //Criando um Scanner
         Scanner ler = new Scanner(System.in);
-        
+        //Gravando algarismos em variáveis criadas
         System.out.println("Informe um número ");
         n1 = ler.nextInt();
         
         System.out.println("Informe outro número ");
         n2 = ler.nextInt();
-        
+        //Gravando a variável a partir de uma operação
         int resultado = n1 + n2;
-        
+        //Resultado
         System.out.println("O resultado é: " + resultado);
         
     }
